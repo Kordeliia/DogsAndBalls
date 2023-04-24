8c5a584db7f1ef0b222f8c4d3fa5a99234ee8aac
Kordeliia <lalvmun@hotmail.com>
2023-04-24 23:26:57 +0200

    Initial commit

---
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30;
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool cond1 = transform.position.x < leftLimit;
+        bool cond2 = transform.position.y < bottomLimit;
+        // Destroy dogs if x position less than left limit
+        if (cond1)
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (cond2)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game overcito");
+            Time.timeScale = 0f;
+        }
+
+    }
+}
